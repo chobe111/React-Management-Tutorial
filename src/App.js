9adbebb3c718ff776752843112a40bea133f8e8b
chobe1 <chobe1@naver.com>
2019-03-19 01:54:47 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,47 @@ import logo from './logo.svg';
 import Customer from './components/Customer';
 import './App.css';
 
-const customer={
+const customers=[
+ {
+  'id':1,
+  'image' : "https://placeimg.com/64/64/1",
   'name':'조명기',
   'birthday':'980719',
   'gender':'남자',
   'job':'대학생'
-
+  
+},
+{
+  'id':2,
+  'image' : "https://placeimg.com/64/64/2",
+  'name':'파커',
+  'birthday':'150819',
+  'gender':'남자',
+  'job':'강아지'  
+},
+{
+  'id':3,
+  'image' : "https://placeimg.com/64/64/3",
+  'name':'유금석',
+  'birthday':'980009',
+  'gender':'남자',
+  'job':'군인'  
 }
+
+]
+
 
 class App extends Component {//웹문서에서 보여지게 될 최소단위
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-  
-      />
+      <div>
+      {
+        customers.map(c=>{
+          return <Customer  key={c.id}  id={c.id}  image={c.image} name={c.name}  birthday={c.birthday} gender={c.gender} job={c.job}
+          />
+        })
+      }
+      </div>
     );
   }
 }
